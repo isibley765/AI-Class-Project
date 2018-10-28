43ac05fa2a0c97c5c7d2116dc1c6696d8d0ff916
Ian Sibley <isibley@purdue.edu>
2018-10-28 03:09:02 -0400

    Able to sort by 'look' degree, trying to make maps of faces and average them

--- ProjMainClasses.py
+++ ProjMainClasses.py
@@ -4,12 +4,14 @@ import numpy as np
 from copy import deepcopy
 from pprint import pprint as pp
 
+from utils.FaceFeatPos import getFace
+
 class FaceBucketMaster:
     def __init__(self, imageList=None, bucketList=None):
         if type(imageList) == list:
             self.images = deepcopy(imageList)
         else:
-            self.images = []
+            self.images = {}
         
         if type(bucketList) == list:
             self.bucketList = deepcopy(bucketList)
@@ -37,18 +39,89 @@ class FaceBucketMaster:
         self.images.append(deepcopy(image))
     
     def takeFolder(self, path):
+        images = []
         for name in os.listdir(path):
             tryImage = os.path.join(path, name)
-            print(tryImage)
-            if name.endswith("jpg"):
-                print(tryImage)
+            if name.endswith((".jpg", ".jpeg", ".png")):
+                images.append({"img": cv2.imread(tryImage), "name": name})
             else:
-                print("Directory?: {}".format(tryImage))
+                # print("Directory?: {}".format(tryImage))
                 if os.path.isdir(os.path.join(path, name)):
-                    self.takeFolder(tryImage)
+                    images += self.takeFolder(tryImage)
+        
+        return images
+
+    """
+        Expects a directory labeled by name, with images inside, possibly in subfolders
+        Moves images to local image repo with L/R/C categories for further processing
+        Doesn't load locally and keep just in RAM, because that blows up into huge memory real fast
+    """
+    def getPeople(self, path):  
+        for name in os.listdir(path):
+            namepath = os.path.join(path, name)
+            if os.path.isdir(namepath):
+                images = self.takeFolder(namepath)
+                self.makeBins(name, images)
+    
+    def makeBins(self, name, imageList):
+        bucket = os.path.join(os.path.join(os.getcwd(), "buckets"))
+        namebucket = os.path.join(bucket, name)
+        pp("Making {}'s training folder".format(name))
+
+        looks = {
+            "SL": os.path.join(namebucket, "SL"),   # Slight left or right
+            "SR": os.path.join(namebucket, "SR"),
+            "L":  os.path.join(namebucket, "L"),    # Normal left, right, or center
+            "R":  os.path.join(namebucket, "R"),
+            "C":  os.path.join(namebucket, "C"),
+            "HL": os.path.join(namebucket, "HL"),   # Hard left or right
+            "HR": os.path.join(namebucket, "HR"),
+        }
+
+        # These need to exist before we make the L, C, and R subfolders
+        if not os.path.exists(bucket):
+            os.makedirs(bucket)
+              
+        if not os.path.exists(namebucket):
+            pp(namebucket)
+            os.makedirs(namebucket)
+
+        for sub in looks.keys():
+            if not os.path.exists(looks[sub]):
+                os.makedirs(looks[sub])
+        
+        for image in imageList:
+            feat = getFace(image["img"])
+
+            pose = feat.findAngle()
+            # pp(pose)
+
+            """
+                Pose indexes correspond to faces found in the image
+                Only one index means only one face found in the image
+                Since this is making our training set, we don't want to identify the desired face just yet
+            """
+            if len(pose) == 1:
+                pose = pose[0]
+                if pose[1] < -0.45:
+                    out = os.path.join(looks["HL"], image["name"])
+                elif pose[1] < -0.20:
+                    out = os.path.join(looks["L"], image["name"])
+                elif pose[1] > 0.45:
+                    out = os.path.join(looks["HR"], image["name"])
+                elif pose[1] > 0.20:
+                    out = os.path.join(looks["R"], image["name"])
+                else:
+                    out = os.path.join(looks["C"], image["name"])
+            
+            cv2.imwrite(out, image["img"])
+
+
 
 if __name__ == "__main__":
     f = FaceBucketMaster()
 
     # f.takeFolder("/home/rovian/Desktop/aligned_images_DB/Abel_Pacheco")
-    f.addImage("/home/rovian/Documents/GitHub/head-pose-estimation/yourface1.jpg")
+    # f.addImage("/home/rovian/Documents/GitHub/head-pose-estimation/yourface1.jpg")
+
+    f.getPeople("./sets/")
--- utils/FaceFeatPos.py
+++ utils/FaceFeatPos.py
@@ -3,88 +3,253 @@ from copy import deepcopy
 import numpy as np
 import argparse
 # import imutils
+import math
 import dlib
 import cv2
 
-class getFace:
-    def __init__(self, image_face=None, predict="./utils/face_landmarks_68.dat", model="./assets/model.txt"):
+
+CNN_INPUT_SIZE = 128
+
+class getFace:  # Presets based on operating in the parent folder of utils, despite this file being located in it
+    # Points based on the outter eye corners and bottom chin point for the default predictor model, please update both together
+    def __init__(self, image_face=None, predict="./utils/shape_predictor_68_face_landmarks.dat", model="./assets/model.txt", points=[8, 36, 45]):
         if type(image_face)==np.ndarray:
-            self.image_face = deepcopy(image_face)
+            self.orig_size = (len(image_face[0]), len(image_face))
+            self.image_face = image_face # cv2.resize(image_face, (CNN_INPUT_SIZE, CNN_INPUT_SIZE))
         else:
             raise ValueError("Please provide an image in the form of a numpy array find its face")
         # pp(image_face)
         
         self.fullmodel = np.loadtxt(model, delimiter=",", dtype=np.float32)
+        self.indexTrio = points
+        self.cropScale = 256/180.0  # for later, using hard values below for now
         
         self.face_detector = dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor(predict)
 
         self.grey = cv2.cvtColor(self.image_face, cv2.COLOR_BGR2GRAY)
-        self.feats = None
 
         self.rvec = None
         self.tvec = None
-    
-    def getFeatures(self):  # For each face found (rect), find the designated points and return them
-        if not self.feats:
-            self.feats = []
-
-            self.rects = self.face_detector(self.grey, 2)
-            
-            # image = deepcopy(self.image_face)
-
-            for rect in self.rects:
-                # (x, y, w, h) = (rec.left(), rec.top(), rec.right() - rec.left(), rec.bottom() - rec.top())
-                # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-                points = np.asarray([[point.x, point.y] for point in self.predictor(self.grey, rect).parts()])
-                
-                """
-                for point in points:
-                    x = point[0]
-                    y = point[1]
-                    cv2.circle(image, (x, y), 1, (0, 0, 255), 2)
-
-                self.show(image)
-                """
-
-                self.feats.append(points)
-
-        return self.feats
-    
-    def findAngle(self):
+        
+        """
+            Some quick calculations for a "camera array"
+            The basic presets based on image dimensions
+            solvePnPRansic also fine tunes for accuracy
+            Most camera matrixes are negligible for solvePnP processes
+        """
         size = (len(self.image_face), len(self.image_face[0]))
         # pp((len(self.image_face), len(self.image_face[0])))
-
         focus = size[1]
         center = (size[1] / 2, size[0] / 2)
-        camera = np.array(
+        self.camera = np.array(
             [[focus, 0, center[0]],
             [0, focus, center[1]],
             [0, 0, 1]], dtype="double")
-        
 
-        if self.rvec is None:
-            pp(self.image_face)
-            (_, rotvec, transvec) = cv2.solvePnP(
-                self.fullmodel, self.image_face, camera, np.zeros((4, 1)))
-            self.rvec = rotvec
-            self.tvec = transvec
-        
-        (_, rvec, tvec) = cv2.solvePnPRansac(
-            self.fullmodel,
-            self.image_face,
-            camera,
-            np.zeros((4, 1)),
-            rvec=self.rvec,
-            tvec=self.tvec,
-            useExtrinsicGuess=True)
-        
-        pp(rvec, self.r_vec)
-
-        return (rvec, tvec)
+        self.feats = self.getFeatures()
     
-    def show(self, image):
+    def getFeatures(self):  # For each face found (rect), find the designated points and return them
+        feats = []
+
+        self.rects = self.face_detector(self.grey, 2)
+        
+        """
+        image = deepcopy(self.image_face)
+        for point in self.fullmodel:
+            x = point[0]
+            y = point[1]
+            cv2.circle(image, (x, y), 1, (0, 255, 0), 2)
+        cv2.rectangle(image, (64, 64), (570, 380), (0, 255, 0), 2)
+        """
+
+        for rect in self.rects:
+
+            points = np.asarray([[point.x, point.y] for point in self.predictor(self.grey, rect).parts()], dtype=np.float32)
+            feats.append(points)
+            
+            """
+            image = deepcopy(self.image_face)
+            (x, y, w, h) = (rect.left(), rect.top(), rect.right() - rect.left(), rect.bottom() - rect.top())
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            for point in points:
+                x = point[0]
+                y = point[1]
+                cv2.circle(image, (x, y), 1, (0, 0, 255), 2)
+
+            self.show(image)
+            """
+
+
+        return feats
+    
+    def findAngle(self):
+        self.rvec = []
+        self.tvec = []
+
+        for i in range(0, len(self.rects)):
+            model = self.fullmodel # self.modelAdjust(self.rects[i])
+            rvec = []
+            tvec = []
+            _, rvec, tvec = cv2.solvePnP(
+                model, self.feats[i], self.camera, np.zeros((4, 1)))
+
+
+            _, rvec, tvec, _ = cv2.solvePnPRansac(
+                model, self.feats[i], self.camera, np.zeros((4, 1)),
+                rvec=rvec,
+                tvec=tvec,
+                useExtrinsicGuess=True)
+            
+            self.rvec.append(np.asarray([angle[0] for angle in rvec]))
+            self.tvec.append(np.asarray([angle[0] for angle in tvec]))
+
+        return self.rvec
+    
+    def modelAdjust(self, size=256): #   Only aimed to center the model on the image, with linear transforms might be useless
+        model = deepcopy(self.fullmodel)
+        # left, top, width, height
+        # (l, t, w, h) = (rect.left(), rect.top(), rect.right() - rect.left(), rect.bottom() - rect.top())
+
+        model[:, 0] = model[:, 0] * size/180 + size/2
+        model[:, 1] = model[:, 1] * size/180 + size/2
+        
+        image = np.zeros([size, size, 3], dtype=np.uint8)
+        for point in model:
+            x = point[0]
+            y = point[1]
+            cv2.circle(image, (x, y), 1, (0, 255, 0), 2)
+        #cv2.rectangle(image, (l, t), (l+w, t+h), (0, 255, 0), 2)
+
+        for index in self.indexTrio:
+            cv2.circle(image, tuple(model[index][:2]), 1, (0, 0, 255), 2)
+
+        self.show(image)
+
+        return model
+    
+    def trioAdjust(self, size=256): #   Only aimed to center the model on the image, with linear transforms might be useless
+        model = self.fullmodel[self.indexTrio]
+
+        model[:, 0] = model[:, 0] * size/180 + size/2
+        model[:, 1] = model[:, 1] * size/180 + size/2
+        
+        """
+        image = np.zeros([size, size, 3], dtype=np.uint8)
+        for point in model:
+            x = point[0]
+            y = point[1]
+            cv2.circle(image, (x, y), 1, (255, 0, 0), 2)
+        #cv2.rectangle(image, (l, t), (l+w, t+h), (0, 255, 0), 2)
+
+        self.show(image)
+        """
+
+        return model
+    
+    def warpFaceFront(self):
+        trio = np.array([[x[0], x[1]] for x in self.trioAdjust()], dtype=np.float32)
+        
+        for i in range(0, len(self.feats)):
+            w = self.rects[i].width()
+            h = self.rects[i].height()
+            center = self.rects[i].center()
+            pp(self.image_face.shape)
+
+
+            wExtend = (w * 256/180) / 2
+            hExtend = (h * 256/180) / 2
+
+            pt1 = (center.x - wExtend, center.y - hExtend)
+            pt2 = (center.x + wExtend, center.y + hExtend)
+
+            chunk = self.grey[pt1[1]:pt2[1], pt1[0]:pt2[0]]
+            chank = self.image_face[pt1[1]:pt2[1], pt1[0]:pt2[0]]
+
+            """
+            image = deepcopy(self.image_face)
+            cv2.rectangle(image, pt1, pt2, (0, 255, 0), 2)
+            for point in self.feats[i]:
+                x = point[0]
+                y = point[1]
+                cv2.circle(image, (x, y), 1, (255, 0, 0), 2)
+            
+            self.show(image)
+            """
+            self.show(chunk)
+
+
+            found3 = self.feats[i][self.indexTrio]
+            pp(found3)
+            pp(trio)
+
+            affineM = cv2.getAffineTransform(found3, trio)
+            pp(affineM)
+            dst = cv2.warpAffine(chunk, affineM, (pt2[1]-pt1[1], pt2[0]-pt1[0]))
+
+            self.show(dst)
+
+    
+
+
+    # TODO: Delete this method
+    # Line variant of box method, by Ian Sibley, baed on the head-pose-estimation github library
+    # Used only for visual appraisal during testing, not for algorithm completion
+    def draw_annotation_line(self, image, color=(255, 255, 255), line_width=2):
+        """Draw a 3D box as annotation of pose"""
+        image = deepcopy(image)
+
+        point_3d = []
+        rear_size = 0
+        rear_depth = 0
+        point_3d.append((0, 0, rear_depth))
+
+        front_size = 100
+        front_depth = 100
+        point_3d.append((0, 0, front_depth))
+
+        point_3d = np.array(point_3d, dtype=np.float).reshape(-1, 3)
+
+        if not self.rvec:
+            self.findAngle()
+            pp("Finding angle")
+
+        for i in range(0, len(self.rvec)):
+            # Map to 2d image points
+            (point_2d, _) = cv2.projectPoints(point_3d,
+                                            self.rvec[i],
+                                            self.tvec[i],
+                                            self.camera,
+                                            np.zeros((4, 1)))
+            point_2d = np.int32(point_2d.reshape(-1, 2))
+
+            # Draw all the lines
+            cv2.polylines(image, [point_2d], True, color, line_width, cv2.LINE_AA)
+
+        return image
+
+        # Line variant of box method, by Ian Sibley, baed on the head-pose-estimation github library
+    def draw_angle_line(self, image, angle, start, color=(255, 255, 255), line_width=2):
+        rad = angle * 100 # rad = math.radians(angle*100)        
+        pointa = start
+        pointb = [pointa[0]+50*math.sin(rad), pointa[0]+50*math.cos(rad)]
+        point_2d = np.asarray([pointa, pointb], dtype=np.int32)
+        pp(point_2d)
+
+        # Draw all the lines
+        cv2.polylines(image, [point_2d], True, color, line_width, cv2.LINE_AA)
+
+        return image
+    
+    def show(self, image=None):
+        try:    # Just in case made equal to undefined value?
+            image
+        except NameError:
+            image = None
+        
+        
+        if type(image) == None:
+            image = self.image_face
         
         cv2.imshow("Output", image)
         cv2.waitKey(20000)
@@ -93,25 +258,30 @@ class getFace:
 
 
 if __name__ == "__main__":
-    img = np.float32(cv2.imread("/home/rovian/Documents/GitHub/neural/Faith/therock.jpg"))
-    try:
+    img = cv2.imread("/home/rovian/Documents/GitHub/head-pose-estimation/self/twoface.jpg")
+    face = getFace(img)
+    face.warpFaceFront()
+    """
+
+    # cv2.circle(img, tuple(face.feats[0][33]), 1, (0, 0, 255), 2) # 33'd index is the tip of the nose
+    face.show(face.draw_annotation_line(img, color=(255, 0, 0)))
+    face.show(img)
+    feats = face.getFeatures()[0]
+
+    for point in feats:
+        x = point[0]
+        y = point[1]
+        cv2.circle(img, (x, y), 1, (0, 0, 255), 2)
+
+    face.show(img)
+    cv2.imwrite("rockout.jpg", img)
+    pp(feats)
+    
+
+    for i in range(0, 9):
+        img = cv2.imread("/home/rovian/Documents/GitHub/head-pose-estimation/self/Ian_Sibley/yourface{}.jpg".format(i))
         face = getFace(img)
-
-        """
-        feats = face.getFeatures()[0]
-
-        for point in feats:
-            x = point[0]
-            y = point[1]
-            cv2.circle(img, (x, y), 1, (0, 0, 255), 2)
-
-        face.show(img)
-        cv2.imwrite("rockout.jpg", img)
-        pp(feats)
-        """
-        pp(face.findAngle())
-    except ValueError as v:
-        # pp(type(cv2.imread("/home/rovian/Documents/GitHub/head-pose-estimation/self/twoface.jpg")))
-        pp(v)
-    except Exception as e:
-        pp(e)
+        pose = face.findAngle()[0]
+        print("u/d: {}\nl/r: {}\n".format(pose[0], pose[1]))
+        face.show(face.draw_annotation_line(img, color=(255, 0, 0)))
+    """
